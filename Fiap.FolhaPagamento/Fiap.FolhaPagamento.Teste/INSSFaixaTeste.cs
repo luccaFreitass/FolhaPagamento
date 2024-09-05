6a65864635296ed9c35b7777e14732dbcb058f33
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 22:36:37 -0300

    Projeto e fase de testes

---
+++ Fiap.FolhaPagamento/Fiap.FolhaPagamento.Teste/INSSFaixaTeste.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.FolhaPagamento.Teste
+{
+    public class INSSFaixaTeste
+    {
+        public static IEnumerable<object> DadosValorFaixa => new List<object[]>()
+        {
+            new object[]{0, 14120.01m, 7.5m, 1412m},
+            new object[]{14120.01m, 2666.68m, 9m, 688}
+        };
+
+        [Fact] //Fact representa um cenario de Teste
+        public void INSS_Faixa_Deve_Conter_Valor()
+        {
+            var faixa = new INSSFaixa
+            {
+                Piso = 0,
+                Teto = 1422
+            };
+
+            Assert.True(faixa.ContemValor(1200));
+        }
+
+       // [Theory]
+        //[MemberData(nameof(DadosValorFaixa))]
+        //public void 
+
+        [Fact]
+        public void Deve_Obter_1412()
+        {
+            var faixa = new INSSFaixa {
+                Piso = 0,
+                Teto = 1412,
+                Aliquota = 7.5m
+            };
+
+            //Act
+            var resultado = faixa.ObterValorFaixa(1412);
+            const decimal esperado = 1412m;
+
+            //Assert
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
+
---
+++ Fiap.FolhaPagamento/Fiap.FolhaPagamento.Teste/InssTeste.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.FolhaPagamento.Teste
+{
+    public class InssTeste
+    {   
+        //Padrão triple A:
+        //Arrange, Act, Assert
+        [Fact]
+        public void Deveria_Aplicar_Faixa_1() { 
+
+            //Arrange: Configura para o teste
+            var inss = new Inss();
+
+            //Act: realiza a ação
+            //1422.00 - 7.5% = 105.9
+            var desconto = inss.CalcularDesconto(1412);
+
+            //Assert: verifica se asserção
+            //está correta
+            Assert.Equal(105.9m, desconto);
+        }
+        [Fact]
+        public void Deveria_Aplicar_Faixa_2()
+        {
+            const decimal salario = 2100.00m;
+            const decimal descontoEsperado = 167.82m;
+            var inss = new Inss();
+
+            //1412 *7.5% = 109.05
+            //[2100 - 1412] * 0.09 = 688,00 * 0.09 = 61,92
+            //105.05 + 61.92 = 167.82
+            var desconto = inss.CalcularDesconto(salario);
+
+            Assert.Equal(descontoEsperado, desconto);
+        }
+    }
+}
